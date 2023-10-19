e8cc97b0614f93929704ce50e88ebb8e41ad1817
rohi <varma36a@gmail.com>
2023-10-19 19:49:40 +0530

    mock data

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,23 +1,37 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input, Output, EventEmitter, SimpleChanges, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent implements AfterViewInit {
 
   @Input() text:string | undefined;
   @Input() color:string | undefined;
+  @Output() btnClick = new EventEmitter();
 
+
+  message:string | undefined;
   constructor() { }
 
   ngOnInit(): void {
     console.log(this.color);
+    console.log(this.btnClick);
+    console.log(this.message);
   }
+  
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['color']) {
+      this.message = `Input changed to: ${changes['color'].currentValue}`;
+    }
+  } 
 
+  ngAfterViewInit() {
+    const button = document.querySelector('#myButton');
+  }
   onClick(){
-    console.log('add');
+    this.btnClick.emit();
   }
 
 }
